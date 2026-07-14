45f6612a624668f04b7ec5eba2cc05f96934e867
dogaes <dogaeski@gmail.com>
2026-07-14 20:15:06 +0300

    feat(user): add User JPA entity mapped to users table

---
+++ src/main/java/com/i2i/CryptoWallet/user/User.java
@@ -0,0 +1,32 @@
+package com.i2i.CryptoWallet.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")          // which table does this map to?
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   // matches BIGSERIAL
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
